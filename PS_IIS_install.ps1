a9e83c0ed3b14fa9b0eb9255640241833c67c736
dimdimuzun <dmitriy.d.uzun@gmail.com>
2021-01-10 23:18:45 +0200

    ready 2 deploy

---
+++ PS_IIS_install.ps1
@@ -0,0 +1,9 @@
+﻿<powershell>
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled false
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+netsh advFirewall Firewall add rule name="OSRadar Rule PING IPv4" protocol=icmpv4:8,any dir=in action=allow
+Rename-Computer -NewName MyIISserver
+</powershell>
